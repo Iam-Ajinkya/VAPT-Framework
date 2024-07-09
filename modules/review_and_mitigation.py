6cbf43d998f2af499d6119021697e678ec591f87
Iam-Ajinkya <ajinkyakamthe004@gmail.com>
2024-07-09 21:21:59 +0530

    review_and_mitigation module is update

---
+++ modules/review_and_mitigation.py
@@ -0,0 +1,14 @@
+import logging
+
+def review_mitigation():
+    logging.info("Reviewing and suggesting mitigation steps")
+    print("Review and mitigation steps here.")
+    logging.info("Review and mitigation completed")
+
+def run():
+    logging.info("Review and mitigation started")
+    review_mitigation()
+    logging.info("Review and mitigation completed")
+
+if __name__ == "__main__":
+    run()
